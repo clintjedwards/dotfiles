b5193b91fa0dcc450884fe1cfb6ce03c398f9e80
Clint J Edwards <clint.j.edwards@gmail.com>
2023-06-13 03:32:42 -0400

    add gum git commit and todo cleanup

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+TYPE=$(gum choose "fix" "feat" "docs" "refactor" "test" "chore" "revert")
+
+# Pre-populate the input with the type(scope): so that the user may change it
+SUMMARY=$(gum input --value "$TYPE: " --placeholder "Summary of this change")
+DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
+
+# Commit these changes
+gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
