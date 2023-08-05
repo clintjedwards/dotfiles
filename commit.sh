2f69442fba4c490c2280e9240555d4faf991412f
Clint J Edwards <clint.j.edwards@gmail.com>
2023-08-04 21:49:09 -0400

    update

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@ TYPE=$(gum choose "fix" "feat" "docs" "refactor" "test" "chore" "revert")
 
 # Pre-populate the input with the type(scope): so that the user may change it
 SUMMARY=$(gum input --value "$TYPE: " --placeholder "Summary of this change")
-DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
+DESCRIPTION=$(gum write --char-limit 0 --placeholder "Details of this change (CTRL+D to finish)")
 
 # Commit these changes
 gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
